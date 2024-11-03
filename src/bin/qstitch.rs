6330c0b0d1a267a4498d0f0298e4b95f2fc7d257
Slope <linearfunc@proton.me>
2024-11-03 22:03:12 +0900

    Began work on cli

--- src/bin/qstitch.rs
+++ src/bin/qstitch.rs
@@ -1,3 +1,18 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Clone, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The images to stitch.
+    ///
+    /// If a directory is provided, then the images contained in that directory will be stitched.
+    images: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("cli");
+    let cli = Cli::parse();
+
+    println!("{:?}", cli.images);
 }
