efbdc8671db198a84ee5ef931b47913a41bc8dae
Slope <linearfunc@proton.me>
2024-11-06 00:00:21 +0900

    Implemented cli interface

--- src/bin/qstitch.rs
+++ src/bin/qstitch.rs
@@ -1,18 +1,32 @@
 use std::path::PathBuf;
+use clap::{Parser, Args};
 
-use clap::Parser;
-
+/// Quickly stitch raws.
+///
+/// A list of images can provided as input, or the `--dir` flag can be used
+/// instead to specify a directory of images to stitch.
 #[derive(Debug, Clone, Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
+    #[clap(flatten)]
+    input: Input,
+    /// The output directory to place the stitched images in.
+    #[clap(long, short)]
+    output: PathBuf,
+}
+
+#[derive(Debug, Clone, Args)]
+#[group(required = true, multiple = false)]
+struct Input {
     /// The images to stitch.
-    ///
-    /// If a directory is provided, then the images contained in that directory will be stitched.
-    images: Vec<PathBuf>,
+    images: Option<Vec<PathBuf>>,
+    /// A directory of images to stitch.
+    #[clap(long, short, alias = "dir")]
+    dir: Option<Vec<PathBuf>>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    println!("{:?}", cli.images);
+    println!("{:?} {:?}", cli.input.images, cli.input.dir);
 }
